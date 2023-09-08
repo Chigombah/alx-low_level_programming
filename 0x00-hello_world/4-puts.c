21a93047d777589f662a512241f38c4a5daa6e9c
Charles Chigozie Mbah <burnicembah@gmail.com>
2023-09-08 15:46:16 -0700

    4-puts.col

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
+#include <conio.h>
 
-/**
- * C program that prints exactly "Programming is like building a multilingual puzzle".
- *
- * Return: 0 (successful)
+
+/* A C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
+
+Use the function puts
+You are not allowed to use printf
+Your program should end with the value 0successful)
  */
 
-int main (void)
+int main ()
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	printf("\" Programming is like building a multilingual puzzle" );
 	return (0);
 }
