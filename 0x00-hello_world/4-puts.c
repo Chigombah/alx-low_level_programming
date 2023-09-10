6873903e29b442e4085b31c2c17d19131e02c650
Charles Chigozie Mbah <burnicembah@gmail.com>
2023-09-10 00:09:04 -0700

    loo

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 
-/*
- * main - prints task "Programming is like building a multilingual puzzle".
-
- * Return: 0 (successful)
- */
+/**
+* main - prints task "Programming is like building a multilingual puzzle".
+*
+* Return: 0 (successful)
+*/
 
 int main(void)
 /* This code below is what prints the task */
