cb79f0acfbe558233b08c0e12b8b33d011a6fe59
Charles Chigozie Mbah <burnicembah@gmail.com>
2023-09-09 23:29:27 -0700

    4-puts.copi

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,15 +2,16 @@
 
 
 
-/* A C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
+/**
+ *	 A C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
 
-Use the function puts
-You are not allowed to use printf
-Your program should end with the value 0successful)
+	 Use the function puts
+	 You are not allowed to use printf
+	 Your program should end with the value 0successful)
  */
 
-int main ()
+	int main(void)
 {
-	printf("\" Programming is like building a multilingual puzzle" );
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
