4f805450e503c47859b2f26bd090f960b3eaf0f8
Charles Chigozie Mbah <burnicembah@gmail.com>
2023-09-09 23:55:34 -0700

    mumi

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,13 @@
 
 
 /*
- * main - prints message "Programming is like building a multilingual puzzle".
+ * main - prints task "Programming is like building a multilingual puzzle".
 
  * Return: 0 (successful)
  */
 
 int main(void)
+
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
